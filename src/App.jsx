1068fe31bccb94dcb4ef402af51ff440bbe3480d
SgtBear1986 <loren.fulton@gmail.com>
2025-06-12 18:14:51 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 export default function App() {
   return (
-    <div style={{ color: "white", padding: "2rem" }}>
+    <div style={{
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#121212"
+    }}>
       <h1>🃏 Blackjack Trainer</h1>
       <p>App code inserted here</p>
     </div>
