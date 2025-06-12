75959e88ebaf9136710183fde42fb17c79479c87
SgtBear1986 <loren.fulton@gmail.com>
2025-06-12 16:32:18 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+
+// ... [Content truncated for brevity in display] ...
+
+export default function BlackjackFlashcards() {
+  // [Function content from canvas]
+}
